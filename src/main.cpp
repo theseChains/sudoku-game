1464f6dfa098172e1e760b3fb7fe266df35dfe3f
theseChains <rasim0103@gmail.com>
2022-09-14 23:04:17 +0300

    Add grid creating function

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+std::ostream& boldOn(std::ostream& outputStream)
+{
+	outputStream << "\e[36;1m";
+
+	return outputStream;
+}
+
+
+std::ostream& boldOff(std::ostream& outputStream)
+{
+	outputStream << "\e[0m";
+
+	return outputStream;
+}
+
+void createThreeByThreeGrid()
+{
+	for (int topLineIndex{ 0 }; topLineIndex < 9; ++topLineIndex)
+	{
+		std::cout << boldOn << "+-";
+	}
+	std::cout << "+\n";
+
+	for (int row{ 0 }; row < 9; ++row)
+	{
+		for (int boxColumn{ 0 }; boxColumn < 9; ++boxColumn)
+		{
+			if (boxColumn == 0 || boxColumn == 3 || boxColumn == 6)
+			{
+				std::cout << boldOn << "| ";
+			}
+			else
+			{
+				std::cout << boldOff << "| ";
+			}
+		}
+		
+		std::cout << boldOn << "|\n";
+
+		for (int lineColumn{ 0 }; lineColumn < 9; ++lineColumn)
+		{
+			if (row == 2 || row == 5 || row == 8)
+			{
+				std::cout << boldOn << "+-";
+			}
+			else if (lineColumn == 0 || lineColumn == 3 || lineColumn == 6)
+			{
+				std::cout << boldOn << '+' << boldOff << '-';
+			}
+			else
+			{
+				std::cout << boldOff << "+-";
+			}
+		}
+
+		std::cout << boldOn << "+\n";
+	}
+}
+
+int main()
+{	
+	createThreeByThreeGrid();
+
+	return 0;
+}
