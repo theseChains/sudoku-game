0575a05b5cbddd1a87c4e499bb6f80a2b96e3e2f
theseChains <rasim0103@gmail.com>
2022-09-19 14:02:32 +0300

    Refactor the print grid function

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ std::ostream& boldOff(std::ostream& outputStream)
 	return outputStream;
 }
 
-void createGrid()
+void printLettersAtTheTop()
 {
 	std::cout << "  ";
 	for (char letterLineIndex{ 'A' }; letterLineIndex < 'J'; ++letterLineIndex)
@@ -36,56 +36,72 @@ void createGrid()
 		std::cout << std::setw(3) << letterLineIndex << ' ';
 	}
 	std::cout << '\n';
-	
+}
+
+void printTopLine()
+{
 	std::cout << "  ";
 	for (int topLineIndex{ 0 }; topLineIndex < 9; ++topLineIndex)
 	{
 		std::cout << boldOn << "+---";
 	}
 	std::cout << "+\n" << boldOff;
+}
+
+void printNonNumberRows(int currentRow)
+{
+	std::cout << "  ";
+	for (int lineColumn{ 0 }; lineColumn < 9; ++lineColumn)
+	{
+		if (currentRow == 2 || currentRow == 5 || currentRow == 8)
+		{
+			std::cout << boldOn << "+---";
+		}
+		else if (lineColumn == 0 || lineColumn == 3 || lineColumn == 6)
+		{
+			std::cout << boldOn << '+' << boldOff << "---"; 
+		}
+		else
+		{
+			std::cout << boldOff << "+---";
+		}
+	}
+	std::cout << boldOn << "+\n" << boldOff;
+}
+
+void printNumberRows(int currentRow)
+{
+	std::cout << currentRow + 1 << ' ';
+	for (int boxColumn{ 0 }; boxColumn < 9; ++boxColumn)
+	{
+		if (boxColumn == 0 || boxColumn == 3 || boxColumn == 6)
+		{
+			// the numbers are to be inserted here
+			std::cout << boldOn << "| " << boldOff << rnd::getNumber() << ' ';
+		}
+		else
+		{
+			std::cout << boldOff << "| " << rnd::getNumber() << ' ';
+		}
+	}
+	std::cout << boldOn << "|\n";
+}
+
+void printGrid()
+{
+	printLettersAtTheTop();
+	printTopLine();
 
 	for (int row{ 0 }; row < 9; ++row)
 	{
-		std::cout << row + 1 << ' ';
-		for (int boxColumn{ 0 }; boxColumn < 9; ++boxColumn)
-		{
-			if (boxColumn == 0 || boxColumn == 3 || boxColumn == 6)
-			{
-				// the numbers are to be inserted here
-				std::cout << boldOn << "| " << boldOff << rnd::getNumber() << ' ';
-			}
-			else
-			{
-				std::cout << boldOff << "| " << rnd::getNumber() << ' ';
-			}
-		}
-		
-		std::cout << boldOn << "|\n";
-		
-		std::cout << "  ";
-		for (int lineColumn{ 0 }; lineColumn < 9; ++lineColumn)
-		{
-			if (row == 2 || row == 5 || row == 8)
-			{
-				std::cout << boldOn << "+---";
-			}
-			else if (lineColumn == 0 || lineColumn == 3 || lineColumn == 6)
-			{
-				std::cout << boldOn << '+' << boldOff << "---"; 
-			}
-			else
-			{
-				std::cout << boldOff << "+---";
-			}
-		}
-
-		std::cout << boldOn << "+\n" << boldOff;
+		printNumberRows(row);	
+		printNonNumberRows(row);
 	}
 }
 
 int main()
 {	
-	createGrid();
+	printGrid();
 
 	return 0;
 }
